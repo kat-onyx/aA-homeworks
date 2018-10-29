d769f4d969d1265ec9ef1af85eca57f8ff17acd2
kat <sarah.kathryn.peters@gmail.com>
2018-10-28 17:56:26 -0700

    Add Simon class with skeleton methods

---
+++ W2D1/simon/lib/simon.rb
@@ -0,0 +1,47 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+
+  end
+
+  def take_turn
+
+    
+  end
+
+  def show_sequence
+    self.add_random_color
+  end
+
+  def require_sequence
+
+  end
+
+  def add_random_color
+    random_color = COLORS.sample
+    @seq << random_color
+  end
+
+  def round_success_message
+
+  end
+
+  def game_over_message
+
+  end
+
+  def reset_game
+    @seq = [] if @game_over == true
+    @game_over = false
+    @sequence_length = 1
+  end
+end

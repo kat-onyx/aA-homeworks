9a72a669d5fb0bdd32e2f6e8f10802e998ab5feb
kat <sarah.kathryn.peters@gmail.com>
2018-10-31 22:37:12 -0700

    Add n log n sorting algorithm for #dominant_octopus

--- W2D4/big_octopus.rb
+++ W2D4/big_octopus.rb
@@ -18,6 +18,48 @@ def sluggish_octopus(fishies)
   longest_fish
 end
 
+# Find the longest fish in O(n log n) time. Hint: You saw a sorting algorithm that runs in O(n log n) in the Sorting Demo.
+# Remember that Big O is classified by the dominant term.
+
+class Array
+  def merge_sort(&prc)
+    prc ||= Proc.new { |x,y| x <=> y }
+
+    return self if self.length == 1
+
+    mid = self.length / 2
+
+    left = self.take(mid).merge_sort(&prc)
+    right = self.drop(mid).merge_sort(&prc)
+
+    Array.merge(left, right, &prc)
+
+  end
+
+  def merge(left, right, &prc)
+    merged = []
+
+    until left.empty? || right.empty?
+      if prc.call(left, right) == 1
+        merged << right
+      elsif prc.call(left, right) == -1
+        merged << left
+      else
+        merged << left
+      end
+    end
+    merged + left + right
+  end
+end
+
+
+def dominant_octopus(fishies)
+  fishies.merge_sort(&prc)[-1]
+end
+
+# Find the longest fish in O(n) time. The octopus can hold on to the longest
+# fish that you have found so far while stepping through the array only once
+
 def clever_octopus(fishies)
   longest_fish = ""
   fishies.each do |fish|

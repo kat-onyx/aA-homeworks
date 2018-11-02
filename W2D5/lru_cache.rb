208365fea8e7a04e4e16cf2c1835a0b9d31cf94a
kat <sarah.kathryn.peters@gmail.com>
2018-11-01 22:14:31 -0700

    Set up #initialize, #count, #add(el), #show

--- W2D5/lru_cache.rb
+++ W2D5/lru_cache.rb
@@ -1,17 +1,30 @@
 class LRUCache
-  def initialize
+  def initialize(size)
+    @size = size
+    @buckets = []
   end
 
   def count
-    # returns number of elements currently in cache
+    @buckets.count
   end
 
   def add(el)
     # adds element to cache according to LRU principle
+    if @buckets.include?(el)
+      @buckets.delete(el)
+      @buckets.push(el)
+    elsif !@buckets.include?(el) && @buckets.length == @size
+      @buckets.shift
+      @buckets.push(el)
+    else
+      @buckets.push(el)
+    end
+    return nil
   end
 
   def show
     # shows the items in the cache, with the LRU item first
+    @buckets
   end
 
   private

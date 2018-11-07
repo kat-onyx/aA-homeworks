e2b5821e1146acb318a2e66f1d6a97e6a0a29cfb
kat <sarah.kathryn.peters@gmail.com>
2018-11-06 23:02:51 -0800

    Hopefully fixing commit...

---
+++ W3D3/db/migrate/20181107065319_create_people_table.rb
@@ -0,0 +1,9 @@
+class CreatePeopleTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :people do |t|
+      t.string :name, null: false
+      t.string :house_id, null: false
+      t.timestamps
+    end
+  end
+end

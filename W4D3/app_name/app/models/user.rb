80b7526080e03073bd8bf6eec486cd2809f7dfa9
kat <sarah.kathryn.peters@gmail.com>
2018-11-13 22:51:49 -0800

    Finish phase 3 and install bcrypt gem

--- W4D3/app_name/app/models/user.rb
+++ W4D3/app_name/app/models/user.rb
@@ -16,4 +16,34 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6, allow_nil: true }
   validates :session_token, presence: true
   after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    @user = User.find_by(username: username)
+    return nil if user.nil?
+    if @user.is_password?(password)
+      return @user
+    else
+      return nil
+    end
+  end
+
+  def self.generate_session_token
+    @session_token = SecureRandom.base64(16)
+  end
+
+  def reset_session_token
+    @session_token = self.class.generate_session_token
+    self.save
+  end
+
+  def ensure_session_token
+    if @session_token.nil?
+      self.class.generate_session_token
+    end
+    @session_token
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
 end

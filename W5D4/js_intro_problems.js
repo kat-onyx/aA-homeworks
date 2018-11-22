174f80fc8c1127aad9c333b7c5b58fd11f0ea348
kat <sarah.kathryn.peters@gmail.com>
2018-11-21 20:42:04 -0800

    Complete mystery scoping

---
+++ W5D4/js_intro_problems.js
@@ -0,0 +1,52 @@
+function mysteryScoping1() {
+  var x = 'out of block';
+  if (true) {
+    var x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+//functionally scoped, no error
+
+function mysteryScoping2() {
+  const x = 'out of block';
+  if (true) {
+    const x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+// constant redefined in inner block will not throw an error.  They're block scoped.
+
+function mysteryScoping3() {
+  const x = 'out of block';
+  if (true) {
+    var x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+// const defined in outer scope and var is scoped per function so there is overlap on delcaring.
+//Cannot redefine const.
+
+function mysteryScoping4() {
+  let x = 'out of block';
+  if (true) {
+    let x = 'in block';
+    console.log(x);
+  }
+  console.log(x);
+}
+// each in their own block, no error.
+
+function mysteryScoping5() {
+  let x = 'out of block';
+  if (true) {
+    let x = 'in block';
+    console.log(x);
+  }
+  let x = 'out of block again';
+  console.log(x);
+}
+
+//let declared twice in the same scope.

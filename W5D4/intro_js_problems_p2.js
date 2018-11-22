e62e2a1bad7555299ac2d1386de33f71fb87d16c
kat <sarah.kathryn.peters@gmail.com>
2018-11-21 22:32:04 -0800

    Complete some of callback function and start elephant protoype

---
+++ W5D4/intro_js_problems_p2.js
@@ -0,0 +1,15 @@
+function titleize(names, cb) {
+  let titleized = names.map(name => name.toUpperCase)
+  cb(titleized)
+}
+//not finished
+
+function Elephant {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet {
+  
+}

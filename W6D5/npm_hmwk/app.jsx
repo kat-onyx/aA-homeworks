fa370759faf415f11312b796523d3f60e3119814
kat <sarah.kathryn.peters@gmail.com>
2018-11-30 08:31:24 -0800

    Set up render function for calc

--- W6D5/npm_hmwk/app.jsx
+++ W6D5/npm_hmwk/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Congrats from './congrats';
+import Calculator from 'calculator';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
--- W6D5/npm_hmwk/calculator.jsx
+++ W6D5/npm_hmwk/calculator.jsx
@@ -3,15 +3,25 @@ import React from 'react';
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    // your code here
+    this.state: {
+      result: 0,
+      num1: "",
+      num2: ""
+    }
+    this.sumNum1 = this.sumNum1.bind(this);
   }
 
-  // your code here
+  sumNum1(e) {
+    e.preventDefault();
+    this.setState({ num1: this.state.num1 })
+  }
 
   render() {
     return (
       <div>
-        <h1>Hello World</h1> // replace this with your code
+        <input onChange=(this.sumNum1)></input>
+        <input onChange=(this.sumNum1.bind(this)></input>
+        <h1>{this.state.result}</h1>
       </div>
     );
   }

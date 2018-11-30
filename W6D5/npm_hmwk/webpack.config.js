c78e04effa010a2a12798fae6043b9e4c9814a0c
kat <sarah.kathryn.peters@gmail.com>
2018-11-29 22:43:48 -0800

    Complete set up for npm

---
+++ W6D5/npm_hmwk/webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: './entry.jsx',
+  output: {
+    filename: './bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions['.js', '.jsx', '*']
+  }
+};
